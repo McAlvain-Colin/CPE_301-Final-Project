152630d87d0dc63ba3645b42ee0e0743abf6c498
McAlvain-Colin <105736965+McAlvain-Colin@users.noreply.github.com>
2023-05-07 20:13:52 -0700

    Update Clock_Embedded_Syetem_Code.ino

--- Final_Project/Clock_Embedded_Syetem_Code/Clock_Embedded_Syetem_Code.ino
+++ Final_Project/Clock_Embedded_Syetem_Code/Clock_Embedded_Syetem_Code.ino
@@ -96,12 +96,12 @@ unsigned int pos3 = 0;
 
 
 //hand Arrays
-unsigned int hand1[] = {0/4, 30/4, 60/4, 90/4, 120/4, 150/4, 180/4, 210/4, 240/4, 270/4, 300/4, 330/4};
-unsigned int hand2[] = {5/4, 35/4, 65/4, 95/4, 125/4, 155/4, 185/4, 215/4, 245/4, 275/4, 305/4, 335/4};
-unsigned int hand3[] = {10/4, 40/4, 70/4, 100/4, 130/4, 160/4, 190/4, 220/4, 250/4, 280/4, 310/4, 340/4};
-unsigned int hand4[] = {15/4, 45/4, 75/4, 105/4, 135/4, 165/4, 195/4, 225/4, 255/4, 285/4, 315/4, 345/4};
-unsigned int hand5[] = {20/4, 50/4, 80/4, 110/4, 140/4, 170/4, 200/4, 230/4, 260/4, 290/4, 320/4, 350/4};
-unsigned int hand6[] = {25/4, 55/4, 85/4, 115/4, 145/4, 175/4, 205/4, 235/4, 265/4, 295/4, 325/4, 355/4};
+unsigned int hand1[] = {0, 30, 60, 90, 120, 10000, 180, 210, 240, 270, 300, 330};
+unsigned int hand2[] = {5, 35, 65, 95, 125, 10005, 185, 21000, 245, 275, 305, 335};
+unsigned int hand3[] = {10, 40, 70, 100, 130, 160, 190, 220, 250, 280, 310, 340};
+unsigned int hand4[] = {1000, 45, 75, 105, 135, 165, 195, 225, 255, 285, 31000, 345};
+unsigned int hand5[] = {20, 50, 80, 110, 140, 170, 200, 230, 260, 290, 320, 350};
+unsigned int hand6[] = {25, 55, 85, 11000, 145, 175, 205, 235, 265, 295, 325, 355};
 
 unsigned int* hands[] = {hand1, hand2, hand3, hand4, hand5, hand6};
 
@@ -144,6 +144,9 @@ void setup() {
   *ddr_h = 0xFF;
   *ddr_L = 0xFF;
   // set ports to input
+  // clockHand1Servo.write(90);   
+  // clockHand2Servo.write(90);   
+  // clockHand3Servo.write(90);   
 
   // run test function
   //test();
@@ -165,15 +168,15 @@ void loop() {
   // unsigned int positionChoice = getPositionChoice();
 
   // unsigned int positionChange = getNewHandPostion(armChoice, positionChoice);
+  workLight();
 
   printTempChoice();
 
-  unsigned char cs1;
-  while (U0kbhit()==0){}; // wait for RDA = true
-  cs1 = U0getchar();
-  U0putchar(cs1);
+  workLight();
 
   moveHandTemp();
+  
+  workLight();
 }
 
 
@@ -198,27 +201,27 @@ void test(){
   // test servo
   for (pos = 0; pos <= 180; pos += 1) { 
     clockHand1Servo.write(pos);              
-    delay(15); 
+    delay(1000); 
   }
   for (pos = 0; pos <= 180; pos += 1) {   
     clockHand2Servo.write(pos);              
-    delay(15);
+    delay(1000);
   }
   for (pos = 0; pos <= 180; pos += 1) { 
     clockHand3Servo.write(pos);              
-    delay(15);                       
+    delay(1000);                       
   }
   for (pos = 180; pos >= 0; pos -= 1) { 
     clockHand1Servo.write(pos);              
-    delay(15);   
+    delay(1000);   
   }
   for (pos = 0; pos >= 180; pos += 1) { 
     clockHand2Servo.write(pos);              
-    delay(15);  
+    delay(1000);  
   } 
   for (pos = 0; pos >= 180; pos += 1) { 
     clockHand3Servo.write(pos);              
-    delay(15);                       
+    delay(1000);                       
   }  
 }
 
@@ -369,19 +372,19 @@ unsigned int getNewHandPostion(unsigned int armChoice, unsigned int posChoice){
 //     if(armChoice == 1){
 //       for (pos = 0; pos <= posChange; pos += 1) { 
 //         clockHand1Servo.write(pos);              
-//         delay(15);  
+//         delay(1000);  
 //       }
 //     }
 //     else if(armChoice == 2){
 //       for (pos = 0; pos <= posChange; pos += 1) { 
 //         clockHand2Servo.write(pos);              
-//         delay(15); 
+//         delay(1000); 
 //       }
 //     }
 //     else if(armChoice == 3){
 //       for (pos = 0; pos <= posChange; pos += 1) { 
 //         clockHand3Servo.write(pos);              
-//         delay(15); 
+//         delay(1000); 
 //       }
 //     }
 //   }
@@ -389,382 +392,224 @@ unsigned int getNewHandPostion(unsigned int armChoice, unsigned int posChoice){
 //     if(armChoice == 1){
 //       for (pos = 0; pos >= posChange; pos += 1) { 
 //         clockHand1Servo.write(pos);              
-//         delay(15);  
+//         delay(1000);  
 //       }
 //     }
 //     else if(armChoice == 2){
 //       for (pos = 0; pos >= posChange; pos += 1) { 
 //         clockHand2Servo.write(pos);              
-//         delay(15); 
+//         delay(1000); 
 //       }
 //     }
 //     else if(armChoice == 3){
 //       for (pos = 0; pos >= posChange; pos += 1) { 
 //         clockHand3Servo.write(pos);              
-//         delay(15); 
+//         delay(1000); 
 //       }
 //     }
 //   }
 // }
 void moveHandTemp(){
-  unsigned char temp = U0getchar();
-  if(temp == 'a' && (pos1 <= hand1[0])){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == 'a' && pos1 >= hand1[0]){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-
-
-  else if(temp == 'b' && pos1 <= hand1[1]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'b' && pos1 >= hand1[1]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'c' && pos1 <= hand1[2]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'c' && pos1 >= hand1[2]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'd' && pos1 <= hand1[3]){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == 'd' && pos1 >= hand1[3]){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-
-
-  else if(temp == 'e' && pos1 <= hand1[4]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'e' && pos1 >= hand1[4]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'f' && pos1 <= hand1[5]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'f' && pos1 >= hand1[5]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'g' && pos1 <= hand1[6]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'g' && pos1 >= hand1[6]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'h' && pos1 <= hand1[7]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'h' && pos1 >= hand1[7]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'i' && pos1 <= hand1[8]){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == 'i' && pos1 >= hand1[8]){
-      clockHand1Servo.write(pos);              
-      delay(15);  
-  }
-
-
-  else if(temp == 'j' && pos1 <= hand1[9]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'j' && pos1 >= hand1[9]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'k' && pos1 <= hand1[10]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'k' && pos1 >= hand1[10]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'l' && pos1 <= hand1[11]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'l' && pos1 >= hand1[11]){
-      clockHand1Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'm' && pos2 <= hand2[0]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'm' && pos2 >= hand2[0]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'n' && pos2 <= hand2[1]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == 'n' && pos2 >= hand2[1]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
-  }
-
-
-  else if(temp == 'o' && pos2 <= hand2[2]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'o' && pos2 >= hand2[2]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'p' && pos2 <= hand2[3]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'p' && pos2 >= hand2[3]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == 'q' && pos2 <= hand2[4]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'q' && pos2 >= hand2[4]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-
   
-  else if(temp == 'r' && pos2 <= hand2[5]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+  while (U0kbhit()==0){}; // wait for RDA = true
+  unsigned char temp = U0getchar();
+  U0putchar(temp);
+  if(temp == 'a'){
+      clockHand1Servo.write(hand1[0]/4);              
+      delay(1000);  
   }
-  else if(temp == 'r' && pos2 >= hand2[5]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+
+  else if(temp == 'b'){
+      clockHand1Servo.write(hand1[1]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 's' && pos2 <= hand2[6]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
+  else if(temp == 'c'){
+      clockHand1Servo.write(hand1[2]/4);              
+      delay(1000); 
   }
-  else if(temp == 's' && pos2 >= hand2[6]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
+  
+
+
+  else if(temp == 'd'){
+      clockHand1Servo.write(hand1[3]/4);              
+      delay(1000);  
   }
 
 
-  else if(temp == 't' && pos2 <= hand2[7]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 't' && pos2 >= hand2[7]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'e'){
+      clockHand1Servo.write(hand1[4]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 'u' && pos2 <= hand2[8]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'u' && pos2 >= hand2[8]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'f'){
+      clockHand1Servo.write(hand1[5]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 'v' && pos2 <= hand2[9]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'v' && pos2 >= hand2[9]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'g'){
+      clockHand1Servo.write(hand1[6]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 'w' && pos2 <= hand2[10]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == 'w' && pos2 >= hand2[10]){
-      clockHand2Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'h'){
+      clockHand1Servo.write(hand1[7]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 'x' && pos2 <= hand2[11]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
+  else if(temp == 'i'){
+      clockHand1Servo.write(hand1[8]/4);              
+      delay(1000);  
   }
-  else if(temp == 'x' && pos2 >= hand2[11]){
-      clockHand2Servo.write(pos);              
-      delay(15);  
+
+  else if(temp == 'j'){
+      clockHand1Servo.write(hand1[9]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'k'){
+      clockHand1Servo.write(hand1[10]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'l'){
+      clockHand1Servo.write(hand1[11]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'm'){
+      clockHand2Servo.write(hand2[0]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'n'){
+      clockHand2Servo.write(hand2[1]/4);              
+      delay(1000);  
   }
 
 
-  else if(temp == 'y' && pos3 <= hand3[0]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'o'){
+      clockHand2Servo.write(hand2[2]/4);              
+      delay(1000); 
   }
-  else if(temp == 'y' && pos3 >= hand3[0]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+
+  else if(temp == 'p'){
+      clockHand2Servo.write(hand2[3]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == 'z' && pos3 <= hand3[1]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'q'){
+      clockHand2Servo.write(hand2[4]/4);              
+      delay(1000); 
   }
-  else if(temp == 'z' && pos3 >= hand3[1]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  
+  else if(temp == 'r'){
+      clockHand2Servo.write(hand2[5]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 's'){
+      clockHand2Servo.write(hand2[6]/4);              
+      delay(1000);  
+  }
+
+  else if(temp == 't'){
+      clockHand2Servo.write(hand2[7]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'u'){
+      clockHand2Servo.write(hand2[8]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'v'){
+      clockHand2Servo.write(hand2[9]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == 'w'){
+      clockHand2Servo.write(hand2[10]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '1' && pos3 <= hand3[2]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == '1' && pos3 >= hand3[2]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'x'){
+      clockHand2Servo.write(hand2[11]/4);              
+      delay(1000);  
   }
 
 
-  else if(temp == '2' && pos3 <= hand3[3]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == 'y'){
+      clockHand3Servo.write(hand3[0]/4);              
+      delay(1000); 
   }
-  else if(temp == '2' && pos3 >= hand3[3]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+
+  else if(temp == 'z'){
+      clockHand3Servo.write(hand3[1]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '3' && pos3 <= hand3[4]){
-      clockHand3Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == '3' && pos3 >= hand3[4]){
-      clockHand3Servo.write(pos);              
-      delay(15);  
+  else if(temp == '1'){
+      clockHand3Servo.write(hand3[2]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '4' && pos3 <= hand3[5]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == '4' && pos3 >= hand3[5]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == '2'){
+      clockHand3Servo.write(hand3[3]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '5' && pos3 <= hand3[6]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == '5' && pos3 >= hand3[6]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == '3'){
+      clockHand3Servo.write(hand3[4]/4);              
+      delay(1000);  
   }
 
 
-  else if(temp == '6' && pos3 <= hand3[7]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == '4'){
+      clockHand3Servo.write(hand3[5]/4);              
+      delay(1000); 
   }
-  else if(temp == '6' && pos3 >= hand3[7]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+
+  else if(temp == '5'){
+      clockHand3Servo.write(hand3[6]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == '6'){
+      clockHand3Servo.write(hand3[7]/4);              
+      delay(1000); 
+  }
+
+  else if(temp == '7'){
+      clockHand3Servo.write(hand3[8]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '7' && pos3 <= hand3[8]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == '8'){
+      clockHand3Servo.write(hand3[9]/4);              
+      delay(1000);  
   }
-  else if(temp == '7' && pos3 >= hand3[8]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+
+  else if(temp == '9'){
+      clockHand3Servo.write(hand3[10]/4);              
+      delay(1000); 
   }
 
 
-  else if(temp == '8' && pos3 <= hand3[9]){
-      clockHand3Servo.write(pos);              
-      delay(15);  
-  }
-  else if(temp == '8' && pos3 >= hand3[9]){
-      clockHand3Servo.write(pos);              
-      delay(15);  
-  }
-
-
-  else if(temp == '9' && pos3 <= hand3[10]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
-  }
-  else if(temp == '9' && pos3 >= hand3[10]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
-  }
-
-
-  else if(temp == '0' && pos3 <= hand3[11]){
-      clockHand3Servo.write(pos);              
-      delay(15);
-  }
-  else if(temp == '0' && pos3 >= hand3[11]){
-      clockHand3Servo.write(pos);              
-      delay(15); 
+  else if(temp == '0'){
+      clockHand3Servo.write(hand3[11]/4);              
+      delay(1000);
   }
 }
 
@@ -908,3 +753,13 @@ void USART0_Flush()
 }
 
 
+void workLight(){
+  unsigned int data = 0; 
+  data = adc_read(1);
+  if(data > 15){
+    *port_b |= 0x80;
+  }
+  else{
+    *port_b |= 0x00;
+  }
+}
