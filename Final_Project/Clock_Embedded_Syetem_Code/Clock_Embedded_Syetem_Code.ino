75455a2b0438c262c74139de853846f4cf1b16bf
McAlvain-Colin <105736965+McAlvain-Colin@users.noreply.github.com>
2023-05-07 20:57:22 -0700

    Update Clock_Embedded_Syetem_Code.ino

--- Final_Project/Clock_Embedded_Syetem_Code/Clock_Embedded_Syetem_Code.ino
+++ Final_Project/Clock_Embedded_Syetem_Code/Clock_Embedded_Syetem_Code.ino
@@ -123,6 +123,15 @@ unsigned char printTempChar[46] = "Select arm and position. [a-z][0-9]: ";
 
 unsigned int data = 0; 
 
+// LCD variables
+const int rs = 12;
+const int en = 11;
+const int d4 = 5;
+const int d5 = 4;
+const int d6 = 3;
+const int d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   // setup the UART
   U0init(9600);
@@ -144,9 +153,15 @@ void setup() {
   *ddr_h = 0xFF;
   *ddr_L = 0xFF;
   // set ports to input
-  // clockHand1Servo.write(90);   
-  // clockHand2Servo.write(90);   
-  // clockHand3Servo.write(90);   
+  clockHand1Servo.write(90);   
+  clockHand2Servo.write(90);   
+  clockHand3Servo.write(90);   
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+
+  lcd.print("hello, world!");
 
   // run test function
   //test();
@@ -172,11 +187,15 @@ void loop() {
 
   printTempChoice();
 
-  workLight();
+  //workLight();
 
   moveHandTemp();
+
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
   
-  workLight();
+  //workLight();
 }
 
 
@@ -754,12 +773,40 @@ void USART0_Flush()
 
 
 void workLight(){
-  unsigned int data = 0; 
+  float compare = 10;
+  float data = 0; 
   data = adc_read(1);
-  if(data > 15){
+  print_int(data);
+  if(data > compare){
+    U0putchar('a');
     *port_b |= 0x80;
   }
   else{
-    *port_b |= 0x00;
+    U0putchar('b');
+    *port_b = 0x00;
   }
+}
+
+void my_delay(unsigned int freq)
+{
+  // calc period
+  double period = 1.0/double(freq);
+  // 50% duty cycle
+  double half_period = period/ 2.0f;
+  // clock period def
+  double clk_period = 0.0000000625;
+  // calc ticks
+  unsigned int ticks = half_period / clk_period;
+  // stop the timer
+  *myTCCR1B &= 0xF8;
+  // set the counts
+  *myTCNT1 = (unsigned int) (65536 - ticks);
+  // start the timer
+  * myTCCR1B |= 0b00000001;
+  // wait for overflow
+  while((*myTIFR1 & 0x01)==0); // 0b 0000 0000
+  // stop the timer
+  *myTCCR1B &= 0xF8;   // 0b 0000 0000
+  // reset TOV
+  *myTIFR1 |= 0x01;
 }
